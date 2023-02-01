779a8b44a5e627660aafc5e334ac8225fccf25ae
DJ Holt <dj@holt.dj>
2023-02-01 13:18:52 -0700

    Lecture 2/1

--- pyserver/server.py
+++ pyserver/server.py
@@ -1,9 +1,27 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+
+MY_MOVIES = ["Avatar", "Inception", "Oppenheimer"]
 
 class MyRequestHandler(BaseHTTPRequestHandler):
 
-    # TODO
-    pass
+    def handleNotFound(self):
+        self.send_response(404)
+        self.send_header("Content-Type", "text/plain")
+        self.end_headers()
+        self.wfile.write(bytes("Not found.", "utf-8"))
+
+    def do_GET(self):
+        if self.path == "/movies":
+            # response status code:
+            self.send_response(200)
+            # response header:
+            self.send_header("Content-Type", "application/json")
+            self.end_headers()
+            # response body:
+            self.wfile.write(bytes(json.dumps(MY_MOVIES), "utf-8"))
+        else:
+            self.handleNotFound()
 
 
 def run():
